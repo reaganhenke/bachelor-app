015873c344dcc48d4f7b428b2f3e6f52bdc37e2c
Reagan Henke <reaganhenke@gmail.com>
2021-02-23 22:51:12 -0500

    update envs, bold, and footer links

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  max_week: 8,
+  contestants: {
+    spreadsheetId: '1P3EC0_IEVvS5FerbPJ7swBT3S5cy2jFjPCzM3gc4Xmc',
+    worksheetId: 1,
+  },
 };
