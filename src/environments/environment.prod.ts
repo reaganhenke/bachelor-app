0ef4730117d1be113fa33417410538c10e8e146d
Reagan Henke <reaganhenke@gmail.com>
2021-02-25 11:36:42 -0500

    update max week to 9

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  max_week: 8,
+  max_week: 9,
   contestants: {
     spreadsheetId: '1P3EC0_IEVvS5FerbPJ7swBT3S5cy2jFjPCzM3gc4Xmc',
     worksheetId: 1,
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  max_week: 8,
+  max_week: 9,
   contestants: {
     spreadsheetId: '1P3EC0_IEVvS5FerbPJ7swBT3S5cy2jFjPCzM3gc4Xmc',
     worksheetId: 1,
